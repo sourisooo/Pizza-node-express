98a9b9fe41b648354d18bf14bbb56c7102892990
sourisooo <119761249+sourisooo@users.noreply.github.com>
2023-12-04 19:27:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,6 @@ app.use(express.static('public'));
 app.get("/", (req, res) => {
 
 
-  // res.setHeader('Content-Type', 'text/html; charset=utf-8');
-
-  // res.send(`
-
-  //   <ul>
-
-  //   ${recipes.map( e => `<li> <a href=/recipes/${e.name} >${e.name} </a></li>`)}
-
-  //   </ul>
-
-  //   `); 
 
   res.render('recipes', {recipes})
 
@@ -35,12 +24,11 @@ const {recipe} = req.params;
 
  let rindexFound = recipes.findIndex( e => {return e.name.toLowerCase()===recipe.toLowerCase()} );
 
- let images=[]; images.push('/img/Leonardo_Diffusion_XL_attractive_hot_pizza_incentive_commercia_0.jpg','/img/Leonardo_Diffusion_XL_attractive_hot_pizza_incentive_commercia_1.jpg','/img/Leonardo_Diffusion_XL_attractive_hot_pizza_incentive_commercia_2.jpg','/img/Leonardo_Diffusion_XL_attractive_hot_pizza_incentive_commercia_3jpg');
 
   res.render('recipe', {recipeFound,rindexFound})
 
 
-  images.forEach(image =>   res.sendFile(image))
+
 
 
 })
